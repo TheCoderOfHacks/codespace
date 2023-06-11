ea91bec91942e2015252b7c1ddd0d48270a14fe8
TheCoderOfHacks <102878085+TheCoderOfHacks@users.noreply.github.com>
2023-06-11 19:26:27 +0000

    Sun, Jun 11, 2023, 7:26 PM +00:00

--- idk.c
+++
@@ -1,9 +0,0 @@
-#include <cs50.h>
-#include <stdio.h>
-
-int main(void)
-{
-    string first = get_string("Whats your first name? ");
-    string last = get_string("Whats you last name? ");
-    printf("hello, %s %s\n", first, last);
-}
