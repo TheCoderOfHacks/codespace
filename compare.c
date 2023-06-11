93a11b3314d37b3809b7cdb01baa7630c83e0ba0
TheCoderOfHacks <102878085+TheCoderOfHacks@users.noreply.github.com>
2023-06-11 20:43:06 +0000

    Sun, Jun 11, 2023, 8:43 PM +00:00

--- compare.c
+++ compare.c
@@ -4,4 +4,18 @@
 int main(void)
 {
    int x = get_int("what is x ");
+   int y = get_int("what is y ");
+
+   if (x < y)
+   {
+        printf("x is less than y\n");
+   }
+   else if (x > y)
+   {
+        printf("x is not less than y\n");
+   }
+   else
+   {
+        printf("x is equal to y\n");
+   }
 }
