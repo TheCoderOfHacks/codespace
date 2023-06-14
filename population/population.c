0c4c63648ba818d4c1baca90dc0889b2fc218c53
TheCoderOfHacks <102878085+TheCoderOfHacks@users.noreply.github.com>
2023-06-14 01:29:41 +0000

    Wed, Jun 14, 2023, 1:29 AM +00:00

--- population/population.c
+++ population/population.c
@@ -9,15 +9,15 @@ int main(void)
     {
         s = get_int("Start Size: ");
     }
-    while (s < 9);
+    while (s < 10);
     // TODO: Prompt for end size
-    int e;
+    float e;
     {
         e = get_int("End Size: ");
     }
     while (e < s);
     // TODO: Calculate number of years until we reach threshold
-    float y = 0;
+    int y = 0;
     while (s < e)
     {
         s = s + (s /3) - (s / 4);
